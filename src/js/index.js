f9a96c9db52363f51c96049e8a3d7447adf91222
Danilo Gomes <danilohenriquegomes1@gmail.com>
2024-05-28 00:12:26 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+//objetivo 1
+
+const botoes = document.querySelectorAll('botao');
+
+botoes.forEach(botao =>{
+    console.log(botao);
+})
+
+botoes.addEventListener("click", () => {
+    console.log(botao);
+    
+});
+
